ea4fca43e1713e131540a5c538d6e7c4c161e5fe
gianluca <perronegian@hotmail.com>
2020-04-28 19:09:28 +0800

    Added delete option for note

--- app/src/main/java/edu/cs50/harvard/notes/NoteActivity.java
+++ app/src/main/java/edu/cs50/harvard/notes/NoteActivity.java
@@ -1,10 +1,16 @@
 package edu.cs50.harvard.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class NoteActivity extends AppCompatActivity {
 
@@ -17,9 +23,44 @@ public class NoteActivity extends AppCompatActivity {
         setContentView(R.layout.activity_note);
 
         editText = findViewById(R.id.note_edit_text);
-        String content = getIntent().getStringExtra("content");
-        int id = getIntent().getIntExtra("id", 0);
+        final String content = getIntent().getStringExtra("content");
+        final int id = getIntent().getIntExtra("id", 0);
         editText.setText(content);
+
+        final NotesDatabase database = MainActivity.database;
+
+        final NotesAdapter adapter = new NotesAdapter();
+
+        FloatingActionButton deleteButton = findViewById(R.id.delete_button);
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which){
+                            case DialogInterface.BUTTON_POSITIVE:
+                                //Yes button clicked
+                                database.noteDao().delete(id);
+                                adapter.reload();
+                                finish();
+                                break;
+
+                            case DialogInterface.BUTTON_NEGATIVE:
+                                //No button clicked
+                                break;
+                        }
+                    }
+                };
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+                builder.setMessage("Are you sure?").setPositiveButton("Yes", dialogClickListener)
+                        .setNegativeButton("No", dialogClickListener).show();
+
+            }
+        });
+
+
     }
 
     @Override
